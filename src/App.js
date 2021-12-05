6e2f4ab94c79621b03c8c5a327ca59844d5f8dff
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-12-05 19:31:15 +0100

    first commmit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Home from "./Home";
+import Navbar from './Navbar';
+import { useState } from 'react';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => {
+
+  return (
+    <div className="app">
+      <h2>
+        App component
+      </h2>
+    </div>
+  );
+}
+ 
+export default App;
