c2fa7f8c3833a36ee1893a00b668b9e24814cae9
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-15 21:29:44 +0100

    function ality

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const [quotes, setQuotes] = useState(null);
     const [isLoading, setIsLoading,] = useState(true);
     const [error, setError] = useState(false);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/quote")
+         .then(res => {
+             if(!res === "ok") {
+             }
+         })
+    })
 
     return (
         <div className="home">
