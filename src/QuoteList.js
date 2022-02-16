e0fb368a949d06ca9baef15d8920fea9f578c3f7
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-16 18:10:35 +0100

    added template to the new component

--- src/QuoteList.js
+++ src/QuoteList.js
@@ -1,7 +1,11 @@
 const QuoteList = () => {
     return (
         <div className="quote-list">
-            
+           {quotes.map(quote => (
+                <div className="quote-preview" key={quote.id}>
+                    <p className="quote-body">{ quote.body }</p>
+                </div>
+            ))} 
         </div>
     );
 }
