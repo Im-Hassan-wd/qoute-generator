f92f60ee97b7dbf48ec3d538424937627f1f19bd
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-16 18:07:35 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import Home from "./Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App = () => {
 
   return (
-    <div className="app">
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="app">
+        <div className="content">
+          <Home />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
  
